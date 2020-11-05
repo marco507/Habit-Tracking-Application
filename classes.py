f0109619b045f6906dc048331b28e56326576a93
Marco Schweiss <schweiss.marco@protonmail.com>
2020-11-05 17:59:37 +0100

    commit

--- classes.py
+++ classes.py
@@ -2,15 +2,15 @@
 import sqlite3
 import login
 import analytics
-from datetime import date
+from datetime import date, timedelta, datetime
+
 
 #Class for creating, deleting and checking habits.
 class Habit(object):
        
     def __init__(self):
         #Assign the stored username to the username attribute 
-        self.__user = login.User()
-        self.__username = self.__user.whoami()
+        self.__username = login.User.whoami()
         #Establish the database connection and cursor
         self.__connection = sqlite3.connect('database.db')
         self.__db = self.__connection.cursor() 
@@ -65,34 +65,129 @@ class Habit(object):
         else:    
             print("The habit " + name + " does not exists")
         
-
-    #Hier aufgehört am 04.11 Gehört noch bearbeitet!!!
+    #Method for checking a habit
     def check(self, name):
-        #Establish DB connection
-        connection = sqlite3.connect('database.db')
-        # Create a cursor for Database Interaction
-        db = connection.cursor()
+    
+        #Check if the habit exists
+        if self.__check_existence(name):
+
+            #Check if an data entry for today already exists
+             self.__db.execute('''SELECT * FROM trackingdata WHERE CheckDate = ?''', (date.today(),))
+             exists =  self.__db.fetchall()
+
+            #If the habit is already checked return an error message
+             if exists:
+                 print("The habit is already checked for today")
+
+             else:
+                #Return the HabitID from the database
+                self.__db.execute('''SELECT HabitID FROM habits WHERE HabitName = ? AND User = ?''', (name, self.__username))
+                result = self.__db.fetchall()
+
+                #Extracting the numeric value from the returned list of tuples
+                for i in result:
+                    for j in i:
+                        habit_id = j
+
+                #Check if the streak is interrupted
+                #Return the period of the habit from the database
+                self.__db.execute('''SELECT Period FROM habits WHERE HabitName = ? AND User = ?''', (name, self.__username))
+                result = self.__db.fetchall()
+
+                #Extracting the value for the period from the returned list of tuples
+                for i in result:
+                    for j in i:
+                        period = j
+
+                #Return the last tracking data entry of the habit
+                self.__db.execute('''SELECT MAX(CheckDate) FROM trackingdata WHERE HabitID = ?''', (habit_id,))
+                result = self.__db.fetchall()
+
+                #Extract the date string from the returned list of tuples
+                for i in result:
+                    for j in i:
+                        last_date = j
+
+                #Convert the string into a datetime object and then into an date object
+                last_date = datetime.strptime(last_date, '%Y-%m-%d')
+                last_date = last_date.date()
+
+                #Calculate the difference between the last checked date and todays date
+                date_difference = date.today() - last_date
+
+                #Check the date difference against the habit period
+                #For period the Daily the date difference must be exactly 1 day
+                if period == "Daily":
+                    #If the date difference is 1 day, increment the streak by 1
+                    if date_difference.days == 1:
+                        self.__db.execute('''UPDATE habits SET CurrentStreak = CurrentStreak + 1 WHERE HabitID = ?''', (habit_id,))
+                    #If the date difference is > 1 set the streak to zero
+                    else:
+                        self.__db.execute('''UPDATE habits SET CurrentStreak = 0 WHERE HabitID = ?''', (habit_id,))
+
+                #INSERT a new entry in the tracking data table
+                self.__db.execute('''INSERT INTO trackingdata VALUES(NULL, ?, ?)''',  (date.today(), habit_id))
+                
+
+        #Give back an error message if the habit does not exists     
+        else:    
+            print("The habit " + name + " does not exists")
+
+        #Commit the changes
+        self.__connection.commit()
+        #Close the connection
+        self.__connection.close() 
+
+    def test(self, name):
+        self.__db.execute('''SELECT Period FROM habits WHERE HabitName = ? AND User = ?''', (name, self.__username))
+        result = self.__db.fetchall()
+
+        #Extracting the value for the period from the returned list of tuples
+        for i in result:
+            for j in i:
+                period = j
 
         #Return the HabitID from the database
-        db.execute('''SELECT HabitID FROM habits WHERE HabitName = ? AND User = ?''', (name, self.username))
-        result = db.fetchall()
+        self.__db.execute('''SELECT HabitID FROM habits WHERE HabitName = ? AND User = ?''', (name, self.__username))
+        result = self.__db.fetchall()
 
         #Extracting the numeric value from the returned list of tuples
         for i in result:
             for j in i:
                 habit_id = j
         
-        #INSERT a new entry in the tracking data table
-        db.execute('''INSERT INTO trackingdata VALUES(NULL, ?, ?)''',  (date.today(), habit_id))
+        #Return the last tracking data entry of the habit
+        self.__db.execute('''SELECT MAX(CheckDate) FROM trackingdata WHERE HabitID = ?''', (habit_id,))
+        result = self.__db.fetchall()
 
-        #Check if the streak is broken
+        #Extract the date string from the returned list of tuples
+        for i in result:
+            for j in i:
+                last_date = j
 
+        #Convert the string into a datetime object and then into an date object
+        last_date = datetime.strptime(last_date, '%Y-%m-%d')
+        last_date = last_date.date()
 
+        #Calculate the difference between the last checked date and todays date
+        date_difference = date.today() - last_date
 
-        #Commit the changes
-        connection.commit()
+        #Check the date difference against the habit period
+        #For period the Daily the date difference must be exactly 1 day
+        if period == "Daily":
+            #If the date difference is 1 day, increment the streak by 1
+            if date_difference.days == 1:
+                self.__db.execute('''UPDATE habits SET CurrentStreak = CurrentStreak + 1 WHERE HabitID = ?''', (habit_id,))
+            #If the date difference is > 1 set the streak to zero
+            else:
+                self.__db.execute('''UPDATE habits SET CurrentStreak = 0 WHERE HabitID = ?''', (habit_id,))
+        
+        self.__connection.commit()
+
+        #Stop am 05.11
+
         #Close the connection
-        connection.close() 
+        self.__connection.close() 
 
 
     
--- login.py
+++ login.py
@@ -5,19 +5,26 @@ import os
 #Class for storing and returning user credentials
 class User(object):
     
-    #Store a username in the credentials.txt
+    #Store an username in the credentials.txt file
     def login(self, username):
         credentials = open("credentials.txt", "w")
         credentials.write(username)
         credentials.close()
         print("Logged in succesfully")
         
-    def whoami(self):
-        credentials = open("credentials.txt", "r")
-        user = credentials.read()
-        credentials.close
-        return user
+    #Give back the current user
+    @staticmethod
+    def whoami():
+        #Check if the credentials.txt exist and return the username
+        if os.path.exists("credentials.txt"):
+            credentials = open("credentials.txt", "r")
+            user = credentials.read()
+            credentials.close
+            return user
+        else:
+            print("No user logged in")
         
+    #Delete the username from the credentials.txt file
     def logout(self):
         #Check if the database file exists and delete it
         if os.path.exists("credentials.txt"):
