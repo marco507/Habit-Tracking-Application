880d5bd613f5a73504cd755e399df3e15505cbfb
Marco Schweiss <schweiss.marco@protonmail.com>
2020-11-13 18:32:16 +0100

    commit

--- test_tracker.py
+++ test_tracker.py
@@ -19,13 +19,20 @@ class TestClasses(unittest.TestCase):
         result = self.db.fetchall()
         return result[0][0]
 
+
     #Test for the create method
     def test_create(self):
         #Create a new instance of the class method
         habit = classes.Habit()
 
-        #Enter a predefined habit with the create method
+        #First test an entry with a false period value
+        habit.create("False-Period", "FalseValue")
+        #If the conditional statement is working, no data entry with the name "False-Period" should be found in the database
+        self.assertFalse(self.select_data("False-Period"))
+        
+        #Enter a daily and weekly habit with the create method
         habit.create("Daily-Habit", "Daily")
+        habit.create("Weekly-Habit, Weekly")
 
         #Search for the inserted habit from the Database and check if the values match
         self.assertEqual("Daily-Habit", self.select_data("Daily-Habit"))
