144cd46bd2188b691b135b58ab60d61853ef9448
Marco Schweiss <schweiss.marco@protonmail.com>
2020-11-22 14:53:34 +0100

    Database Check Before Execution

--- setup.py
+++ setup.py
@@ -77,80 +77,89 @@ class Database:
     @staticmethod
     def testdata():
 
-        # Define the default habits
-        default_habits = {
-            "Workout": "Daily",
-            "Shopping": "Weekly",
-            "Cleaning": "Weekly",
-            "Studying": "Daily",
-            "Reading": "Daily",
-        }
-        # Define the creation date of the default habits as 01.10.2020
-        default_date = date(2020, 10, 1)
+        # Check if the database is initialized
+        if os.path.exists("database.db"):
 
-        # Login as testuser
-        login.User.login("testuser")
+            # Define the default habits
+            default_habits = {
+                "Workout": "Daily",
+                "Shopping": "Weekly",
+                "Cleaning": "Weekly",
+                "Studying": "Daily",
+                "Reading": "Daily",
+            }
+            # Define the creation date of the default habits as 01.10.2020
+            default_date = date(2020, 10, 1)
 
-        # Create a new instane of the Habit() class
-        testdata = classes.Habit()
+            # Login as testuser
+            login.User.login("testuser")
 
-        # Insert the default habits with Habit.create() method
-        for i in default_habits:
-            testdata.create(i, default_habits[i], default_date)
+            # Create a new instane of the Habit() class
+            testdata = classes.Habit()
 
-        # Insert random generated tracking data with the Habit.check() method
+            # Insert the default habits with Habit.create() method
+            for i in default_habits:
+                testdata.create(i, default_habits[i], default_date)
 
-        # Define the start date and end date for the random entries
-        start_date = date(2020, 10, 1)
-        end_date = date(2020, 10, 31)
+            # Insert random generated tracking data with the Habit.check() method
 
-        # Weigh the possibility for a streak break at 25% for daily habits
-        possibility_daily = ["A"] * 75 + ["B"] * 25
+            # Define the start date and end date for the random entries
+            start_date = date(2020, 10, 1)
+            end_date = date(2020, 10, 31)
 
-        # Weigh the possibility for a streak break at 25% for weekly habits
-        # and the chance to check a habit before a week has passed at 15%
-        possibility_weekly = ["A"] * 60 + ["B"] * 25 + ["C"] * 15
+            # Weigh the possibility for a streak break at 25% for daily habits
+            possibility_daily = ["A"] * 75 + ["B"] * 25
 
-        # Loop through all default habits
-        for i in default_habits:
-            # Set the entry_date to start_date
-            entry_date = start_date
+            # Weigh the possibility for a streak break at 25% for weekly habits
+            # and the chance to check a habit before a week has passed at 15%
+            possibility_weekly = ["A"] * 60 + ["B"] * 25 + ["C"] * 15
 
-            # Fill in the tracking data
-            while entry_date < end_date:
-                # Check the habit at the entry_date
-                testdata.check(i, entry_date)
+            # Loop through all default habits
+            for i in default_habits:
+                # Set the entry_date to start_date
+                entry_date = start_date
 
-                # Generate new random date
-                # Check if the period is daily or weekly
-                if default_habits[i] == "Daily":
-                    # Choice A = Streak continues = 1 day timediffernence
-                    if choice(possibility_daily) == "A":
-                        entry_date = entry_date + timedelta(days=1)
-                    # Choice B = Streak is broken = random 2 or 3 day timedifference
+                # Fill in the tracking data
+                while entry_date < end_date:
+                    # Check the habit at the entry_date
+                    testdata.check(i, entry_date)
+
+                    # Generate new random date
+                    # Check if the period is daily or weekly
+                    if default_habits[i] == "Daily":
+                        # Choice A = Streak continues = 1 day timediffernence
+                        if choice(possibility_daily) == "A":
+                            entry_date = entry_date + timedelta(days=1)
+                        # Choice B = Streak is broken = random 2 or 3 day timedifference
+                        else:
+                            entry_date = entry_date + \
+                                timedelta(days=randint(2, 3))
+
                     else:
-                        entry_date = entry_date + timedelta(days=randint(2, 3))
+                        # Choice A = Streak continues = 7 day timediffernence
+                        if choice(possibility_weekly) == "A":
+                            entry_date = entry_date + timedelta(days=7)
+                        # Choice B = Streak is broken = random 8 to 10 day timedifference
+                        elif choice(possibility_weekly) == "B":
+                            entry_date = entry_date + \
+                                timedelta(days=randint(8, 10))
+                        # Choice C = Streak continous = random 4 to 6 day timedifference
+                        else:
+                            entry_date = entry_date + \
+                                timedelta(days=randint(4, 6))
 
-                else:
-                    # Choice A = Streak continues = 7 day timediffernence
-                    if choice(possibility_weekly) == "A":
-                        entry_date = entry_date + timedelta(days=7)
-                    # Choice B = Streak is broken = random 8 to 10 day timedifference
-                    elif choice(possibility_weekly) == "B":
-                        entry_date = entry_date + \
-                            timedelta(days=randint(8, 10))
-                    # Choice C = Streak continous = random 4 to 6 day timedifference
-                    else:
-                        entry_date = entry_date + timedelta(days=randint(4, 6))
+                # Set the last tracking entry at the end date (31.10.2020)
+                testdata.check(i, end_date)
 
-            # Set the last tracking entry at the end date (31.10.2020)
-            testdata.check(i, end_date)
+            # Logout
+            login.User.logout()
 
-        # Logout
-        login.User.logout()
+            # Print a success message
+            print("Entered default data")
 
-        # Print a success message
-        print("Entered default data")
+        # Return an error message
+        else:
+            print("Please initialize the database")
 
 
 if __name__ == "__main__":
