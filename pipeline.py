03a6cf4090a7c1c39d25814c21cdbe908c2c7e60
marco507 <72571317+marco507@users.noreply.github.com>
2020-12-05 10:10:06 +0100

    Final Improvement

--- pipeline.py
+++ pipeline.py
@@ -3,8 +3,6 @@ import analytics
 import user
 
 # Class for wrapping all functionality for the command line interface
-
-
 class Pipeline(object):
     def __init__(self):
         self.manage = habits.Habit()
